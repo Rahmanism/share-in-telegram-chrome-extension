7e59e45403aaf5ed38e0c667fa0e18d2a43e75d0
Rahmanism <s.mostafa.rahmani@gmail.com>
2020-05-02 21:45:10 +0430

    It works fine now, without opening a new tab.

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 chrome.tabs.getSelected(null, function(tab) {
     var tg = "tg://msg_url?url=" + tab.url + '&text=' + tab.title;
-    chrome.tabs.create({
-        "url": tg
-    });
+    chrome.tabs.sendMessage(tab.id, tg)
+    // chrome.tabs.create({
+    //     "url": tg
+    // });
 });

19b694d4e67ff2198daeab51ef6ceeffece0c504
Rahmanism <s.mostafa.rahmani@gmail.com>
2020-05-02 22:05:08 +0430

    Now we don't need the index.html because we use background script.

--- js.js
+++ js.js
@@ -1,7 +1,4 @@
-chrome.tabs.getSelected(null, function(tab) {
-    var tg = "tg://msg_url?url=" + tab.url + '&text=' + tab.title;
+chrome.browserAction.onClicked.addListener(function (tab) {
+    var tg = "tg://msg_url?url=" + tab.url + '&text=' + tab.title
     chrome.tabs.sendMessage(tab.id, tg)
-    // chrome.tabs.create({
-    //     "url": tg
-    // });
-});
+})

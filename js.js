e4503038263f34000572aabbae5a0d7470ea2cf6
Rahmanism <rahmani@mailkh.com>
2020-07-06 11:44:07 +0430

    Now we have Share in Telegram in the context menu of links in pages.

--- js.js
+++ js.js
@@ -2,3 +2,13 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     var tg = "tg://msg_url?url=" + tab.url + '&text=' + tab.title
     chrome.tabs.sendMessage(tab.id, tg)
 })
+
+chrome.contextMenus.removeAll()
+chrome.contextMenus.create({
+    title: 'Share in Telegram...',
+    contexts: ['link'],
+    onclick: function (url, tab) {
+        var tg = 'tg://msg_url?url=' + url.linkUrl
+        chrome.tabs.sendMessage(tab.id, tg)
+    }
+})
